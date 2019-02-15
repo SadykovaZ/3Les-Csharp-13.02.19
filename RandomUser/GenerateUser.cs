b8aee64e6c57797c458e8c5cbea263d186852cf4
СадыковаЖ <@206_5>
2019-02-15 11:35:27 +0600

    ++

---
+++ RandomUser/GenerateUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomUser.Model;
+
+namespace RandomUser
+{
+    class GenerateUser
+    {
+        public results GetUser()
+        {
+            string URL = "https://randomuser.me/api/?results=10";
+            ServiceUser serUser = new ServiceUser(URL);
+
+            serUser.GetUsers();
+
+            Users users = serUser.GetUsers();
+
+            return users.results[0];
+        }
+    }
+}
---
+++ RandomUser/Model/ServiceUser.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomUser.Model
+{
+    public class ServiceUser
+    {
+        public ServiceUser(string URL)
+        {
+            this.URL = URL;
+        }
+
+        public string URL { get; set; }
+
+        /// <summary>
+        /// Метод возвращает список пользователей
+        /// </summary>
+        /// <returns></returns>
+        public Users GetUsers()
+        {
+            string json = GetJsonUser();
+            return JsonConvert.DeserializeObject<Users>(json);
+        }
+        private string GetJsonUser()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                return reader.ReadToEnd();
+
+                //Console.WriteLine(json);
+
+              
+            }
+
+           
+        }
+
+    }
+}
